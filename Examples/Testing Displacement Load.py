1 - Dynamic Analysis has its own reaction calculation method that includes inertial forces 2 - Several bug fixes

Examples/Testing Displacement Load.py
@@ -1,9 +1,12 @@
 import numpy
+import scipy.interpolate
+from scipy.interpolate import interp1d
 
 from PyNite import FEModel3D
 model = FEModel3D()
 node_num = 1;
-from numpy import linspace, sqrt
+from numpy import linspace, sqrt, pi, sin
+
 
 z = linspace(0.3,2.7,8)
 for i in z:
@@ -48,15 +51,15 @@ for node in model.Nodes.values():
 model.def_support('A',support_DY=True, support_DZ=True, support_RZ=True,support_DX=True)
 model.def_support('C',support_DZ=True, support_RY=True, support_DX=True)
 
-model.add_node_load('C','FY',3000, case='H')
+model.add_node_load('C','FY',3000, case='P')
 weight = 9.81 * rho * A
 model.add_member_dist_load('AB', 'FZ',weight,weight,case='W')
 model.add_member_dist_load('AB', 'FZ',weight,weight,case='W')
 
 
-model.add_load_combo('COMB1',{'H':1})
+model.add_load_combo('COMB1',{'P':1})
 
-model.def_load_profile('Case 1', [0,0.01],[1,1])
+model.def_load_profile('P', [0,1],[0,1])
 
 
 
@@ -94,36 +97,37 @@ values = 9.81 * numpy.array(values)
 
 # Create a 2D array with 'values' in the first row and 'time' in the second row
 ground_acceleration = numpy.vstack((time, values))
-#model.def_node_disp('C','DY',0.02)
-model.analyze_modal(num_modes=5,sparse=True)
+
+model.analyze_modal(num_modes=20,sparse=True)
 
 #print(model.MASS_PARTICIPATION_PERCENTAGES())
 damping = dict(constant_modal_damping = 0.02)
-
-print(model.analyze_harmonic(harmonic_combo='COMB1',f1 = 40, f2=150,f_div=1000,sparse=True,
-                       damping_options=damping))
-"""model.analyze_linear_time_history_newmark_beta( analysis_method='direct',
-                                            combo_name='COMB1',
-                                            step_size=0.005,
-                                            response_duration=30,
-                                            damping_options=damping,
-                                            log=False, sparse=True)"""
+"""print(model.analyze_harmonic(harmonic_combo='COMB1',f1 = 50, f2=150,f_div=1000,sparse=True,
+                       damping_options=damping))"""
+"""
+model.def_node_disp('T2','DY',0.2)
+model.def_disp_profile('T2','DY',[0,0.1,1],[1,1,1])
+"""
+print(model.analyze_linear_time_history_newmark_beta( analysis_method='direct',
+                                                      combo_name='COMB1',
+                                            step_size=0.5,
+                                            response_duration=1,
+                                            log=False, sparse=True))
 #print(model.NATURAL_FREQUENCIES())
-
+"""
 import matplotlib.pyplot as plt
 
 # Sample data
 #x = model.TIME_THA()
 #dof = model.Nodes['T'].ID * 6 + 3
-#dof2 = model.Nodes['T2'].ID * 6 + 0
-dof2 = model.Nodes['A'].ID * 6 + 2
+dof2 = model.Nodes['T2'].ID * 6 + 1
 #y = model.DISPLACEMENT_THA()[dof2,:]
 # Create a line plot
 #plt.plot(x, y)
 
 x2 = model.LoadFrequencies
-y2 = sqrt(model.REACTIONS_REAL()[dof2,:]**2 + model.REACTIONS_IMAGINARY()[dof2,:]**2)
-#y2 = sqrt(model.VELOCITY_REAL()[dof2,:]**2 + model.VELOCITY_IMAGINARY()[dof2,:]**2)
+y2 = sqrt(model.DISPLACEMENT_REAL()[dof2,:]**2 + model.DISPLACEMENT_IMAGINARY()[dof2,:]**2)
+
 plt.plot(x2,y2)
 # Add labels and a title
 plt.xlabel('X-axis')
@@ -132,15 +136,18 @@ plt.title('Simple Line Plot')
 
 
 
-print(model.REACTIONS_REAL()[:,0])
+#print(model.NATURAL_FREQUENCIES())
 # Show the plot
-plt.show()
+plt.show() """
 
 """
 from PyNite.Visualization import render_model
 render_model(model,
              deformed_scale=2,
              deformed_shape=True,
-             render_loads=True,
+             render_loads=False,
              annotation_size=0.05,
-             combo_name='Combo 1') """
+             combo_name='Combo 1')
+"""
+
+

PyNite/Analysis.py
@@ -1,9 +1,11 @@
 from math import isclose, ceil
 
+from numpy.lib.function_base import interp, gradient
 from scipy.sparse import csc_matrix
 
+from PyNite import FEModel3D
 from PyNite.LoadCombo import LoadCombo
-from numpy import array, atleast_2d, zeros, diag, ones, identity
+from numpy import array, atleast_2d, zeros, diag, ones, identity, linspace
 from numpy.linalg import solve
 import logging
 
@@ -778,6 +780,342 @@ def _partition_D(model):
     # Return the indices and the known displacements
     return D1_indices, D2_indices, D2
 
+
+def _D2(model:FEModel3D,time,num_points = 1000):
+    """Builds a list with known nodal displacements and with the positions in global stiffness
+        matrix of known and unknown nodal displacements
+
+    :return: A list of the global matrix indices for the unknown nodal displacements (D1_indices). A
+                list of the global matrix indices for the known nodal displacements (D2_indices). A list
+                of the known nodal displacements (D2).
+    :rtype: list, list, list
+    """
+
+    D2 = []  # A list of the values of the known nodal displacements (D != None)
+    V2 = []  # A list of the values of the known nodal velocities (V != None)
+    A2 = []  # A list of the values of the known nodal accelerations (A != None)
+    profile_values= model.DisplacementProfiles
+    profile_keys = model.DisplacementProfiles.keys()
+    # Create the auxiliary table
+    for node in model.Nodes.values():
+
+        # Unknown displacement DX
+        if node.support_DX==False and node.EnforcedDX == None:
+            # In future the function _partition() and this function can be merged. Some of the code from
+            # _partition() can be put here
+            pass
+        # Known displacement DX
+        elif node.EnforcedDX != None:
+            if node.name in profile_keys and profile_values[node.name].direction == 'DX':
+                node_name = node.name
+
+                # Original time and displacement profile array
+                time_array = profile_values[node_name].time
+                displacement_profile = profile_values[node_name].profile
+
+                # To get the velocity and acceleration arrays, we need the arrays to be closely spaced
+                # inorder to carry out proper numerical differentiation
+                # Hence we generate new arrays from the originals by interpolation
+                new_time_array = linspace(min(time_array),max(time_array),num_points)
+                new_disp_profile = interp(new_time_array,time_array,displacement_profile)
+
+                # The velocity profile is found by differentiating the new displacement profile
+                velocity_profile = gradient(new_disp_profile,new_time_array)
+
+                # The acceleration profile is found by differentiating the velocity profile
+                acceleration_profile = gradient(velocity_profile,new_time_array)
+
+                # The displacement at the required time is found by interpolating the original
+                # displacement profile
+                disp_profile_at_time = interp(time,time_array,displacement_profile)
+
+                # The velocity and acceleration at time = time are also found by interpolating
+                # their respective profiles
+                vel_profile_at_time = interp(time, new_time_array, velocity_profile)
+                acc_profile_at_time = interp(time, new_time_array,acceleration_profile)
+
+                # The actual values are then calculated and appended
+                D2.append(disp_profile_at_time*node.EnforcedDX)
+                V2.append(vel_profile_at_time*node.EnforcedDX)
+                A2.append(acc_profile_at_time*node.EnforcedDX)
+            else:
+                # If there is no profile defined, then the displacement is constant
+                # Hence the velocity and acceleration are zeros
+                D2.append(node.EnforcedDX)
+                V2.append(0.0)
+                A2.append(0.0)
+
+        # Support at DX
+        else:
+            D2.append(0.0)
+            V2.append(0.0)
+            A2.append(0.0)
+
+        # Unknown displacement DY
+        if node.support_DY == False and node.EnforcedDY == None:
+            # In future the function _partition() and this function can be merged. Some of the code from
+            # _partition() can be put here
+            pass
+        # Known displacement DY
+        elif node.EnforcedDY != None:
+            if node.name in profile_keys and profile_values[node.name].direction == 'DY':
+                node_name = node.name
+
+                # Original time and displacement profile array
+                time_array = profile_values[node_name].time
+                displacement_profile = profile_values[node_name].profile
+
+                # To get the velocity and acceleration arrays, we need the arrays to be closely spaced
+                # inorder to carry out proper numerical differentiation
+                # Hence we generate new arrays from the originals by interpolation
+                new_time_array = linspace(min(time_array), max(time_array), num_points)
+                new_disp_profile = interp(new_time_array, time_array, displacement_profile)
+
+                # The velocity profile is found by differentiating the new displacement profile
+                velocity_profile = gradient(new_disp_profile, new_time_array)
+
+                # The acceleration profile is found by differentiating the velocity profile
+                acceleration_profile = gradient(velocity_profile, new_time_array)
+
+                # The displacement at the required time is found by interpolating the original
+                # displacement profile
+                disp_profile_at_time = interp(time, time_array, displacement_profile)
+
+                # The velocity and acceleration at time = time are also found by interpolating
+                # their respective profiles
+                vel_profile_at_time = interp(time, new_time_array, velocity_profile)
+                acc_profile_at_time = interp(time, new_time_array, acceleration_profile)
+
+                # The actual values are then calculated and appended
+                D2.append(disp_profile_at_time * node.EnforcedDY)
+                V2.append(vel_profile_at_time * node.EnforcedDY)
+                A2.append(acc_profile_at_time * node.EnforcedDY)
+            else:
+                # If there is no profile defined, then the displacement is constant
+                # Hence the velocity and acceleration are zeros
+                D2.append(node.EnforcedDY)
+                V2.append(0.0)
+                A2.append(0.0)
+
+        # Support at DY
+        else:
+            D2.append(0.0)
+            V2.append(0.0)
+            A2.append(0.0)
+
+        # Unknown displacement DZ
+        if node.support_DZ == False and node.EnforcedDZ == None:
+            # In future the function _partition() and this function can be merged. Some of the code from
+            # _partition() can be put here
+            pass
+        # Known displacement DZ
+        elif node.EnforcedDZ != None:
+            if node.name in profile_keys and profile_values[node.name].direction == 'DZ':
+                node_name = node.name
+
+                # Original time and displacement profile array
+                time_array = profile_values[node_name].time
+                displacement_profile = profile_values[node_name].profile
+
+                # To get the velocity and acceleration arrays, we need the arrays to be closely spaced
+                # inorder to carry out proper numerical differentiation
+                # Hence we generate new arrays from the originals by interpolation
+                new_time_array = linspace(min(time_array), max(time_array), num_points)
+                new_disp_profile = interp(new_time_array, time_array, displacement_profile)
+
+                # The velocity profile is found by differentiating the new displacement profile
+                velocity_profile = gradient(new_disp_profile, new_time_array)
+
+                # The acceleration profile is found by differentiating the velocity profile
+                acceleration_profile = gradient(velocity_profile, new_time_array)
+
+                # The displacement at the required time is found by interpolating the original
+                # displacement profile
+                disp_profile_at_time = interp(time, time_array, displacement_profile)
+
+                # The velocity and acceleration at time = time are also found by interpolating
+                # their respective profiles
+                vel_profile_at_time = interp(time, new_time_array, velocity_profile)
+                acc_profile_at_time = interp(time, new_time_array, acceleration_profile)
+
+                # The actual values are then calculated and appended
+                D2.append(disp_profile_at_time * node.EnforcedDZ)
+                V2.append(vel_profile_at_time * node.EnforcedDZ)
+                A2.append(acc_profile_at_time * node.EnforcedDZ)
+            else:
+                # If there is no profile defined, then the displacement is constant
+                # Hence the velocity and acceleration are zeros
+                D2.append(node.EnforcedDZ)
+                V2.append(0.0)
+                A2.append(0.0)
+
+        # Support at DZ
+        else:
+            D2.append(0.0)
+            V2.append(0.0)
+            A2.append(0.0)
+
+        # Unknown displacement RX
+        if node.support_RX == False and node.EnforcedRX == None:
+            # In future the function _partition() and this function can be merged. Some of the code from
+            # _partition() can be put here
+            pass
+        # Known displacement RX
+        elif node.EnforcedRX != None:
+            if node.name in profile_keys and profile_values[node.name].direction == 'RX':
+                node_name = node.name
+
+                # Original time and displacement profile array
+                time_array = profile_values[node_name].time
+                displacement_profile = profile_values[node_name].profile
+
+                # To get the velocity and acceleration arrays, we need the arrays to be closely spaced
+                # inorder to carry out proper numerical differentiation
+                # Hence we generate new arrays from the originals by interpolation
+                new_time_array = linspace(min(time_array), max(time_array), num_points)
+                new_disp_profile = interp(new_time_array, time_array, displacement_profile)
+
+                # The velocity profile is found by differentiating the new displacement profile
+                velocity_profile = gradient(new_disp_profile, new_time_array)
+
+                # The acceleration profile is found by differentiating the velocity profile
+                acceleration_profile = gradient(velocity_profile, new_time_array)
+
+                # The displacement at the required time is found by interpolating the original
+                # displacement profile
+                disp_profile_at_time = interp(time, time_array, displacement_profile)
+
+                # The velocity and acceleration at time = time are also found by interpolating
+                # their respective profiles
+                vel_profile_at_time = interp(time, new_time_array, velocity_profile)
+                acc_profile_at_time = interp(time, new_time_array, acceleration_profile)
+
+                # The actual values are then calculated and appended
+                D2.append(disp_profile_at_time * node.EnforcedRX)
+                V2.append(vel_profile_at_time * node.EnforcedRX)
+                A2.append(acc_profile_at_time * node.EnforcedRX)
+            else:
+                # If there is no profile defined, then the displacement is constant
+                # Hence the velocity and acceleration are zeros
+                D2.append(node.EnforcedRX)
+                V2.append(0.0)
+                A2.append(0.0)
+
+        # Support at RX
+        else:
+            D2.append(0.0)
+            V2.append(0.0)
+            A2.append(0.0)
+
+        # Unknown displacement RY
+        if node.support_RY == False and node.EnforcedRY == None:
+            # In future the function _partition() and this function can be merged. Some of the code from
+            # _partition() can be put here
+            pass
+        # Known displacement RY
+        elif node.EnforcedRY != None:
+            if node.name in profile_keys and profile_values[node.name].direction == 'RY':
+                node_name = node.name
+
+                # Original time and displacement profile array
+                time_array = profile_values[node_name].time
+                displacement_profile = profile_values[node_name].profile
+
+                # To get the velocity and acceleration arrays, we need the arrays to be closely spaced
+                # inorder to carry out proper numerical differentiation
+                # Hence we generate new arrays from the originals by interpolation
+                new_time_array = linspace(min(time_array), max(time_array), num_points)
+                new_disp_profile = interp(new_time_array, time_array, displacement_profile)
+
+                # The velocity profile is found by differentiating the new displacement profile
+                velocity_profile = gradient(new_disp_profile, new_time_array)
+
+                # The acceleration profile is found by differentiating the velocity profile
+                acceleration_profile = gradient(velocity_profile, new_time_array)
+
+                # The displacement at the required time is found by interpolating the original
+                # displacement profile
+                disp_profile_at_time = interp(time, time_array, displacement_profile)
+
+                # The velocity and acceleration at time = time are also found by interpolating
+                # their respective profiles
+                vel_profile_at_time = interp(time, new_time_array, velocity_profile)
+                acc_profile_at_time = interp(time, new_time_array, acceleration_profile)
+
+                # The actual values are then calculated and appended
+                D2.append(disp_profile_at_time * node.EnforcedRY)
+                V2.append(vel_profile_at_time * node.EnforcedRY)
+                A2.append(acc_profile_at_time * node.EnforcedRY)
+            else:
+                # If there is no profile defined, then the displacement is constant
+                # Hence the velocity and acceleration are zeros
+                D2.append(node.EnforcedRY)
+                V2.append(0.0)
+                A2.append(0.0)
+
+        # Support at RY
+        else:
+            D2.append(0.0)
+            V2.append(0.0)
+            A2.append(0.0)
+
+        # Unknown displacement RZ
+        if node.support_RZ == False and node.EnforcedRZ == None:
+            # In future the function _partition() and this function can be merged. Some of the code from
+            # _partition() can be put here
+            pass
+        # Known displacement RZ
+        elif node.EnforcedRX != None:
+            if node.name in profile_keys and profile_values[node.name].direction == 'RZ':
+                node_name = node.name
+
+                # Original time and displacement profile array
+                time_array = profile_values[node_name].time
+                displacement_profile = profile_values[node_name].profile
+
+                # To get the velocity and acceleration arrays, we need the arrays to be closely spaced
+                # inorder to carry out proper numerical differentiation
+                # Hence we generate new arrays from the originals by interpolation
+                new_time_array = linspace(min(time_array), max(time_array), num_points)
+                new_disp_profile = interp(new_time_array, time_array, displacement_profile)
+
+                # The velocity profile is found by differentiating the new displacement profile
+                velocity_profile = gradient(new_disp_profile, new_time_array)
+
+                # The acceleration profile is found by differentiating the velocity profile
+                acceleration_profile = gradient(velocity_profile, new_time_array)
+
+                # The displacement at the required time is found by interpolating the original
+                # displacement profile
+                disp_profile_at_time = interp(time, time_array, displacement_profile)
+
+                # The velocity and acceleration at time = time are also found by interpolating
+                # their respective profiles
+                vel_profile_at_time = interp(time, new_time_array, velocity_profile)
+                acc_profile_at_time = interp(time, new_time_array, acceleration_profile)
+
+                # The actual values are then calculated and appended
+                D2.append(disp_profile_at_time * node.EnforcedRZ)
+                V2.append(vel_profile_at_time * node.EnforcedRZ)
+                A2.append(acc_profile_at_time * node.EnforcedRZ)
+            else:
+                # If there is no profile defined, then the displacement is constant
+                # Hence the velocity and acceleration are zeros
+                D2.append(node.EnforcedRZ)
+                V2.append(0.0)
+                A2.append(0.0)
+
+        # Support at RZ
+        else:
+            D2.append(0.0)
+            V2.append(0.0)
+            A2.append(0.0)
+
+    D2 = array(D2, ndmin=2).T
+    V2 = array(V2, ndmin=2).T
+    A2 = array(A2, ndmin=2).T
+    return D2, V2, A2
+
 def _renumber(model):
     """
     Assigns node and element ID numbers to be used internally by the program. Numbers are
@@ -1188,6 +1526,7 @@ def _transient_solver_linear_direct(K, M, d0, v0, F0, F,
             TIME[i+1] = current_time
 
     return TIME, D, V, A
+
 
 
 